665b5ffeb65925a757b322f72c429cdaa0a41518
WangPinChieh <pp8101313@hotmail.com>
2021-07-18 14:36:42 +0800

    add CarouselIndicatorButton component

--- src/index.js
+++ src/index.js
@@ -6,13 +6,24 @@ const CarouselTypes = {
     Text: 'Text',
     Image: 'Image',
 }
-function CarouselIndicatorButton(props){
+
+function CarouselControlButton(props) {
     return (
-        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to={props.slideTo}
+        <button className={"carousel-control-" + props.direction} type="button" data-bs-target={props.target}
+                data-bs-slide={props.direction}>
+            <span className={"carousel-control-" + props.direction + "-icon"} aria-hidden="true"></span>
+            <span className="visually-hidden">{(props.direction === 'prev' ? 'Previous' : 'Next')}</span>
+        </button>
+    );
+};
+
+function CarouselIndicatorButton(props) {
+    return (
+        <button type="button" data-bs-target={props.target} data-bs-slide-to={props.slideTo}
                 className={(props.active ? "active" : '')}
                 aria-current={(props.current ? 'true' : '')}></button>
     );
-}
+};
 
 class CarouselTextBlock extends React.Component {
     render() {
@@ -69,14 +80,15 @@ class CarouselItem extends React.Component {
         return <CarouselTextBlock/>;
     }
 }
-class Carousel extends React.Component{
+
+class Carousel extends React.Component {
     render() {
         return (
             <div id="myCarousel" className="carousel slide full-height" data-bs-ride="carousel">
                 <div className="carousel-indicators">
-                    <CarouselIndicatorButton slideTo={0} active={true} current={true}/>
-                    <CarouselIndicatorButton slideTo={1}/>
-                    <CarouselIndicatorButton slideTo={2}/>
+                    <CarouselIndicatorButton slideTo={0} target={'#myCarousel'} active={true} current={true}/>
+                    <CarouselIndicatorButton slideTo={1} target={'#myCarousel'}/>
+                    <CarouselIndicatorButton slideTo={2} target={'#myCarousel'}/>
                 </div>
                 <div className="carousel-inner">
                     <CarouselItem active={true} leftBlock={{type: CarouselTypes.Text, text: '<h1>H1 Test</h1>'}}
@@ -86,24 +98,17 @@ class Carousel extends React.Component{
                     <CarouselItem leftBlock={{type: CarouselTypes.Text, text: '<h1>H1 Test</h1>'}}
                                   rightBlock={{type: CarouselTypes.Text, text: '<p>Test</p>'}}/>
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel"
-                        data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#myCarousel"
-                        data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
+                <CarouselControlButton target={'#myCarousel'} direction={'prev'} />
+                <CarouselControlButton target={'#myCarousel'} direction={'next'} />
             </div>
         );
     }
 }
+
 class IntroductionPage extends React.Component {
     render() {
         return (
-            <Carousel />
+            <Carousel/>
         )
     }
 }
